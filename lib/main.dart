fd66cac6d8db346f42e928a3fc7f9fc16061983d
Mina Adly <mina2dly@gmail.com>
2023-04-19 00:13:49 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BirthdayCard());
+}
+
+class BirthdayCard extends StatelessWidget {
+  const BirthdayCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xFFD2BCD5),
+        body: Center(
+          child: Image(
+            image: AssetImage(
+              'images/birthday_card.png',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
